ab525caa3a4211dbd39f5c5edd345fcdf229151b
chasestudioworks-prog <chasestudioworks@gmail.com>
2025-09-11 12:15:23 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Tone & Toon site loaded');
+// Tone & Toon site script
+
+// Auto-update the copyright year
+document.addEventListener("DOMContentLoaded", () => {
+  const yearSpan = document.getElementById("year");
+  if (yearSpan) {
+    yearSpan.textContent = new Date().getFullYear();
+  }
+});
